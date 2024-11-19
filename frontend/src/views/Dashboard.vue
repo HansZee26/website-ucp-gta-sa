f110feb2c2f3a30f3b83ca9cd04a8b485c245dbe
HansZee26 <yundopid@gmail.com>
2024-11-19 22:52:12 +0700

    Add template frontend

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,220 @@
+<script setup>
+import { useLayout } from '@/layout/composables/layout';
+import { ProductService } from '@/service/ProductService';
+import { onMounted, ref, watch } from 'vue';
+
+const { getPrimary, getSurface, isDarkTheme } = useLayout();
+
+const products = ref(null);
+const chartData = ref(null);
+const chartOptions = ref(null);
+
+const items = ref([
+    { label: 'Add New', icon: 'pi pi-fw pi-plus' },
+    { label: 'Remove', icon: 'pi pi-fw pi-trash' }
+]);
+
+onMounted(() => {
+    ProductService.getProductsSmall().then((data) => (products.value = data));
+    chartData.value = setChartData();
+    chartOptions.value = setChartOptions();
+});
+
+function setChartData() {
+    const documentStyle = getComputedStyle(document.documentElement);
+
+    return {
+        labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+        datasets: [
+            {
+                type: 'bar',
+                label: 'Subscriptions',
+                backgroundColor: documentStyle.getPropertyValue('--p-primary-400'),
+                data: [4000, 10000, 15000, 4000],
+                barThickness: 32
+            },
+            {
+                type: 'bar',
+                label: 'Advertising',
+                backgroundColor: documentStyle.getPropertyValue('--p-primary-300'),
+                data: [2100, 8400, 2400, 7500],
+                barThickness: 32
+            },
+            {
+                type: 'bar',
+                label: 'Affiliate',
+                backgroundColor: documentStyle.getPropertyValue('--p-primary-200'),
+                data: [4100, 5200, 3400, 7400],
+                borderRadius: {
+                    topLeft: 8,
+                    topRight: 8
+                },
+                borderSkipped: true,
+                barThickness: 32
+            }
+        ]
+    };
+}
+
+function setChartOptions() {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const borderColor = documentStyle.getPropertyValue('--surface-border');
+    const textMutedColor = documentStyle.getPropertyValue('--text-color-secondary');
+
+    return {
+        maintainAspectRatio: false,
+        aspectRatio: 0.8,
+        scales: {
+            x: {
+                stacked: true,
+                ticks: {
+                    color: textMutedColor
+                },
+                grid: {
+                    color: 'transparent',
+                    borderColor: 'transparent'
+                }
+            },
+            y: {
+                stacked: true,
+                ticks: {
+                    color: textMutedColor
+                },
+                grid: {
+                    color: borderColor,
+                    borderColor: 'transparent',
+                    drawTicks: false
+                }
+            }
+        }
+    };
+}
+
+const formatCurrency = (value) => {
+    return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+};
+
+watch([getPrimary, getSurface, isDarkTheme], () => {
+    chartData.value = setChartData();
+    chartOptions.value = setChartOptions();
+});
+</script>
+
+<template>
+    <div class="grid grid-cols-12 gap-8">
+        <div class="col-span-12 lg:col-span-6 xl:col-span-3">
+            <div class="card mb-0">
+                <div class="flex justify-between mb-4">
+                    <div>
+                        <span class="block text-muted-color font-medium mb-4">Pengunjung</span>
+                        <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">152</div>
+                    </div>
+                    <div class="flex items-center justify-center bg-blue-100 dark:bg-blue-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
+                        <i class="pi pi-users text-blue-500 !text-xl"></i>
+                    </div>
+                </div>
+                <span class="text-primary font-medium">24 baru </span>
+                <span class="text-muted-color">mengunjungi</span>
+            </div>
+        </div>
+        <div class="col-span-12 lg:col-span-6 xl:col-span-3">
+            <div class="card mb-0">
+                <div class="flex justify-between mb-4">
+                    <div>
+                        <span class="block text-muted-color font-medium mb-4">Hasil DONATE HAHA</span>
+                        <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">Rp.5000.000,00</div>
+                    </div>
+                    <div class="flex items-center justify-center bg-orange-100 dark:bg-orange-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
+                        <i class="pi pi-dollar text-orange-500 !text-xl"></i>
+                    </div>
+                </div>
+                <span class="text-primary font-medium">Naik +56% </span>
+                <span class="text-muted-color">Dari minggu lalu</span>
+            </div>
+        </div>
+        <div class="col-span-12 lg:col-span-6 xl:col-span-3">
+            <div class="card mb-0">
+                <div class="flex justify-between mb-4">
+                    <div>
+                        <span class="block text-muted-color font-medium mb-4">Total Warga</span>
+                        <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">28441</div>
+                    </div>
+                    <div class="flex items-center justify-center bg-cyan-100 dark:bg-cyan-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
+                        <i class="pi pi-users text-cyan-500 !text-xl"></i>
+                    </div>
+                </div>
+                <span class="text-primary font-medium">520 </span>
+                <span class="text-muted-color">Warga Baru</span>
+            </div>
+        </div>
+
+        
+        <div class="col-span-12 xl:col-span-6">
+            <div class="card">
+                <div class="flex items-center justify-between mb-6">
+                    <div class="font-semibold text-xl">Notifikasi</div>
+                    <div>
+                        <Button icon="pi pi-ellipsis-v" class="p-button-text p-button-plain p-button-rounded" @click="$refs.menu1.toggle($event)"></Button>
+                        <Menu ref="menu1" :popup="true" :model="items" class="!min-w-40"></Menu>
+                    </div>
+                </div>
+
+                <span class="block text-muted-color font-medium mb-4">TODAY</span>
+                <ul class="p-0 mx-0 mt-0 mb-6 list-none">
+                    <li class="flex items-center py-2 border-b border-surface">
+                        <div class="w-12 h-12 flex items-center justify-center bg-blue-100 dark:bg-blue-400/10 rounded-full mr-4 shrink-0">
+                            <i class="pi pi-dollar !text-xl text-blue-500"></i>
+                        </div>
+                        <span class="text-surface-900 dark:text-surface-0 leading-normal"
+                            >Dari bang developer
+                            <span class="text-surface-700 dark:text-surface-100">Hari ini uang donate terkumpul banyak makasih semua. server bubar!! <span class="text-primary font-bold">Rp.5000.000,00</span></span>
+                        </span>
+                    </li>
+                    <li class="flex items-center py-2">
+                        <div class="w-12 h-12 flex items-center justify-center bg-orange-100 dark:bg-orange-400/10 rounded-full mr-4 shrink-0">
+                            <i class="pi pi-download !text-xl text-orange-500"></i>
+                        </div>
+                        <span class="text-surface-700 dark:text-surface-100 leading-normal">Uang penarikan dari donate <span class="text-primary font-bold">Rp.5000.000,00</span> Sudah masuk rekening.</span>
+                    </li>
+                </ul>
+
+                <span class="block text-muted-color font-medium mb-4">YESTERDAY</span>
+                <ul class="p-0 m-0 list-none mb-6">
+                    <li class="flex items-center py-2 border-b border-surface">
+                        <div class="w-12 h-12 flex items-center justify-center bg-blue-100 dark:bg-blue-400/10 rounded-full mr-4 shrink-0">
+                            <i class="pi pi-dollar !text-xl text-blue-500"></i>
+                        </div>
+                        <span class="text-surface-900 dark:text-surface-0 leading-normal"
+                            >Keyser Wick
+                            <span class="text-surface-700 dark:text-surface-100">has purchased a black jacket for <span class="text-primary font-bold">$59.00</span></span>
+                        </span>
+                    </li>
+                    <li class="flex items-center py-2 border-b border-surface">
+                        <div class="w-12 h-12 flex items-center justify-center bg-pink-100 dark:bg-pink-400/10 rounded-full mr-4 shrink-0">
+                            <i class="pi pi-question !text-xl text-pink-500"></i>
+                        </div>
+                        <span class="text-surface-900 dark:text-surface-0 leading-normal"
+                            >Jane Davis
+                            <span class="text-surface-700 dark:text-surface-100">has posted a new questions about your product.</span>
+                        </span>
+                    </li>
+                </ul>
+                <span class="block text-muted-color font-medium mb-4">LAST WEEK</span>
+                <ul class="p-0 m-0 list-none">
+                    <li class="flex items-center py-2 border-b border-surface">
+                        <div class="w-12 h-12 flex items-center justify-center bg-green-100 dark:bg-green-400/10 rounded-full mr-4 shrink-0">
+                            <i class="pi pi-arrow-up !text-xl text-green-500"></i>
+                        </div>
+                        <span class="text-surface-900 dark:text-surface-0 leading-normal">Your revenue has increased by <span class="text-primary font-bold">%25</span>.</span>
+                    </li>
+                    <li class="flex items-center py-2 border-b border-surface">
+                        <div class="w-12 h-12 flex items-center justify-center bg-purple-100 dark:bg-purple-400/10 rounded-full mr-4 shrink-0">
+                            <i class="pi pi-heart !text-xl text-purple-500"></i>
+                        </div>
+                        <span class="text-surface-900 dark:text-surface-0 leading-normal"><span class="text-primary font-bold">12</span> users have added your products to their wishlist.</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+module.exports = {
+    darkMode: ['selector', '[class*="app-dark"]'],
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    plugins: [require('tailwindcss-primeui')],
+    theme: {
+        screens: {
+            sm: '576px',
+            md: '768px',
+            lg: '992px',
+            xl: '1200px',
+            '2xl': '1920px'
+        }
+    }
+};
